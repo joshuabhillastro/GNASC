17bf6b612ca99283efd79c1cc8d5bf1ddca6f928
u1190961 <97638578+u1190961@users.noreply.github.com>
2022-01-27 10:49:21 -0700

    Add files via upload

---
+++ plotHRDfromEDR3.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 26 18:42:38 2022
+
+@author: joshhill
+"""
+from astroquery.gaia import Gaia
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+import matplotlib.pyplot as plt
+
+Gaia.ROW_LIMIT = -1 #no limit on rows in gaia data
+Gaia.MAIN_GAIA_TABLE = "gaiaedr3.gaia_source" # Select early Data Release 3
+
+coord = SkyCoord(ra=45, dec=30, unit=(u.degree, u.degree), frame='icrs') #certain part of sky to query
+width = u.Quantity(1, u.deg) #how wide to make the query area
+height = u.Quantity(1, u.deg) #how high to make the query area
+r = Gaia.query_object_async(coordinate=coord, width=width, height=height) #query EDR3
+
+bp_rp = r['bp_rp'].data
+M_g = r['phot_g_mean_mag'].data
+
+plt.figure()
+plt.xlabel('bp-rp')
+plt.ylabel('M_g')
+plt.scatter(bp_rp,M_g)
+
