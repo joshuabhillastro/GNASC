06ecf015268ea070337582070e5f9047b01e881a
u1190961 <97638578+u1190961@users.noreply.github.com>
2022-02-08 18:39:28 -0700

    Add files via upload

--- HGCA_HRD.py
+++ HGCA_HRD.py
@@ -5,7 +5,7 @@ Created on Tue Feb  1 15:35:52 2022
 
 @author: joshhill
 """
-from astropy.io import ascii
+#from astropy.io import ascii
 from astropy.table import Table
 from astroquery.gaia import Gaia
 import numpy as np
@@ -16,16 +16,18 @@ import pandas as pd
 
 source_id_list = pd.read_table("/Users/joshhill/Gaia Data/catalog.dat", delimiter = "|", usecols = [1],dtype = np.int64)
 
-source_id_table = Table.from_pandas(source_id_list)
+source_id_table = source_id_list.to_numpy()
+
+#source_id_table = Table(source_id_tablenp, names = 'source_id')
 
 #print(source_id_table)
 
 Gaia.login(user='jhill01',password='Supernova1!')
 
-send_source_id_table = Gaia.upload_table(upload_resource=source_id_table,table_name='source_ids_2')
-table_source_id = 'user_jhill01.source_ids_2'
+#send_source_id_table = Gaia.upload_table(upload_resource=source_id_table,table_name='source_ids_4')
+table_source_id = 'user_jhill01.source_ids_3'
 
-query = "select top 115345 source_id,bp_rp,phot_g_mean_mag,parallax from gaiadr2.gaia_source, source_ids_2" 
+query = "select top 115345 source_id,bp_rp,phot_g_mean_mag,parallax,col0 from gaiadr2.gaia_source inner join user_jhill01.source_ids_3 on source_id = col0"
 job = Gaia.launch_job(query=query)
 
 r = job.get_results()
