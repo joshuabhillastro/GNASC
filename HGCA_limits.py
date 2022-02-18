be5fa289488cc4fa1e05a573f5bea0bc11993560
u1190961 <97638578+u1190961@users.noreply.github.com>
2022-02-18 10:04:17 -0700

    Add files via upload

--- HGCA_limits.py
+++ HGCA_limits.py
@@ -28,7 +28,7 @@ Gaia.login(user='jhill01',password='Supernova1!')
 #send_source_id_table = Gaia.upload_table(upload_resource=source_id_table,table_name='source_ids_4')
 table_source_id = 'user_jhill01.source_ids_3'
 
-query = "select top 115345 source_id,bp_rp,phot_g_mean_mag,parallax,col0 from gaiadr2.gaia_source inner join user_jhill01.source_ids_3 on source_id = col0"
+query = "select top 115345 source_id,bp_rp,phot_g_mean_mag,parallax,col0 from gaiaedr3.gaia_source inner join user_jhill01.source_ids_3 on source_id = col0"
 job = Gaia.launch_job(query=query)
 
 r = job.get_results()
@@ -43,7 +43,7 @@ M_G = M_g - 5*np.log10(D/10)
 
 h = plt.hist2d(np.log10(D), M_g, bins=300, cmin=2, range = [[0,6],[0,15]], norm=colors.PowerNorm(0.5), zorder=0.5)
 plt.scatter(np.log10(D),M_g,s=.5, color='k', zorder=0)
-plt.xlabel('Parallax')
+plt.xlabel('Distance (Kpc)')
 plt.ylabel('Apparent Magnitude')
 cb = plt.colorbar(h[3], ax=plt.subplot(), pad=0.02)
 cb.set_label('Stellar Density')
