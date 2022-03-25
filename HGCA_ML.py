da725930233fb2633fe375e3d7b24ab068cfc1d6
u1190961 <97638578+u1190961@users.noreply.github.com>
2022-03-25 11:15:02 -0600

    Add files via upload

--- HGCA_ML.py
+++ HGCA_ML.py
@@ -5,25 +5,23 @@ Created on Mon Mar 14 18:16:07 2022
 
 @author: joshhill
 """
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 import numpy as np
-import pandas as pd
-from astropy.table import Table
 from astroquery.gaia import Gaia
 from sklearn.datasets import make_classification
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
+import pandas as pd
+#from astropy.table import Table
+from sklearn import datasets, svm, metrics
+#import matplotlib as mpl
 
-#DR2 query
-query = ("select top 10000 "
-                      "solution_id,astrometric_n_obs_al,astrometric_gof_al,astrometric_primary_flag,"
-                      "astrometric_excess_noise,phot_g_mean_mag,parallax_error,parallax_over_error,bp_rp,phot_g_mean_mag,parallax, "
-                      "matched_observations,duplicated_source,phot_variable_flag "
-                      "from gaiadr2.gaia_source where random_index between 20000000 and 29999999")
+#DR2 source ID
+query = ("select top 31123 "
+                      "solution_id,astrometric_n_obs_al,astrometric_gof_al,astrometric_primary_flag,astrometric_excess_noise,phot_g_mean_mag,parallax_error,parallax_over_error,bp_rp,phot_g_mean_mag,parallax, "
+                      "duplicated_source "
+                      "from gaiaedr3.gaia_source order by source_id")
 job = Gaia.launch_job(query=query)
 r = job.get_results()
 
@@ -36,8 +34,8 @@ g_mean_mag = r['phot_g_mean_mag'].data #Mean magnitude in the G band.
 parallax_error = r['parallax_error'].data
 parallax_over_error = r['parallax_over_error'].data
 
-#Not sure how many perameters we will end up needing, but I figured having them will make it easiser 
 
+#Creating M_G to use with ML
 bp_rp = r['bp_rp'].data
 M_g = r['phot_g_mean_mag'].data
 Par = r['parallax'].data
@@ -45,40 +43,46 @@ d = 1/Par
 D = d*1000
 M_G = M_g - 5*np.log10(D/10)
 
-#I use solution_id for what the practice lab uses as digits
-solution_id = r['solution_id'].data
 
-#I am not sure entirely the need for each line of code here
+#For now I will use parallax for what the practice lab uses as digits
 n_samples = len(Par) 
 imshape = Par[0].shape #not sure about this line
-#data = Par.reshape(n_samples,10000)
+
+#data = Par.reshape((n_samples, 10000))
 n_parms = 3
 data = np.zeros((n_samples,n_parms))
-data[:,0]= Par
-data[:,1]= M_g
-data[:,2]= bp_rp
+data [:,0]= np.nan_to_num(Par)
+data [:,1]= np.nan_to_num(M_g)
+data [:,2]= np.nan_to_num(bp_rp)
 
-# choose your classifier. We choose support vector classification
+table = pd.read_csv("/Users/joshhill/Gaia Data/HGCA_Accel.csv", usecols = [1,2,3])
+bp_rpt = table.iloc[:,1]
+M_gt = table.iloc[:,2]
+part = table.iloc[:,2]
+dt = 1/part
+Dt = dt*1000
+M_Gt = M_gt -5*np.log10(Dt/10)
+print(M_Gt,bp_rpt)
+
+targ = np.zeros(n_samples)
+targ = np.where((M_G<3)&(M_G>-2)&(bp_rp>1.0)&(bp_rp<1.8),1,0)
+print(np.sum(targ))
+
+# choose your classifier. I stuck with the one from the lab, but this is tbd
 classifier = svm.SVC()
 
-
-#Uploading the HGCA acceleration catelog. Ben said that the accelerations are the target 
-#targ = pd.read_table("/Users/joshhill/Gaia Data/HGCA_Accel.csv")  #the acceleration from hipparchus 
-#identify k giants
-targ =np.zeros(n_samples)
-msk = (M_G<3)&(M_G>0)&(bp_rp>1.2)&(bp_rp<1.6)
-targ[msk] = 1
-
-#or
-#targ = np.where((M_G<3)&(M_G>0)&(bp_rp>1.2)&(bp_rp<1.6),1,0)
-#get rid of targ[msk] =1
-
+print(data.shape)
 # split into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(data, targ, test_size=0.5, shuffle=False) # what this line
+X_train, X_test, y_train, y_test = train_test_split(data[:], targ[:],
+                                                    test_size=0.5,shuffle=True)
 n_train = len(y_train)
 n_test = len(y_test)
 
-classifier.fit(X_train, y_train.values.ravel()) #have to add .values.ravel() to squish data
+#X_train = X_train.reshape(1,-1)
+#y_train = y_train.reshape(1,-1)
+
+print(X_train.shape,y_train.shape)
+classifier.fit(X_train, y_train)
 predicted = classifier.predict(X_test)
 
 print('number in test set:',len(y_test))
@@ -87,17 +91,15 @@ print('number correctly classified:',right)
 
 msk = y_test!=predicted
 X_fails,y_fails,y_failspred = X_test[msk],y_test[msk],predicted[msk]
-
-
 X, y = make_classification(random_state=0)
-X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                    random_state=0)
-clf = SVC(random_state=0)
-clf.fit(X_train, y_train)
-SVC(random_state=0)
-predictions = clf.predict(X_test)
-cm = confusion_matrix(y_test, predictions, labels=clf.classes_)
+#X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                  #  random_state=0)
+#clf = SVC(random_state=0)
+#clf.fit(X_train, y_train)
+#SVC(random_state=0)
+#predictions = clf.predict(X_test)
+cm = confusion_matrix(y_test, predicted, labels=classifier.classes_)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm,
-                              display_labels=clf.classes_)
+                              display_labels=classifier.classes_)
 disp.plot()
 plt.show()
