dc72b280d312755d2e2af011726a57a07b91be17
u1339885 <97638499+u1339885@users.noreply.github.com>
2022-06-16 13:38:39 -0600

    Update HGCA_Accel.py

--- HGCA_Accel.py
+++ HGCA_Accel.py
@@ -17,13 +17,17 @@ Table = pd.read_csv("/Users/joshhill/Gaia Data/HGCA_Accel.csv")
 
 r = pd.DataFrame(Table)
 
-bp_rp = r['bp_rp']
-M_g = r['phot_g_mean_mag']
 Par = r['parallax']
-chi2 = r['chi2']
-#print(chi2)
 d = 1/Par
 D = d*1000
+msk = (D>0)&(D<100000) 
+bp_rp = r['bp_rp']
+bp_rp = bp_rp[msk]
+M_g = r['phot_g_mean_mag']
+M_g = M_g[msk]
+Par = Par[msk]
+d = d[msk]
+D = D[msk] 
 M_G = M_g - 5*np.log10(D/10)
 
 h = plt.hist2d(bp_rp, M_G, bins=300, cmin=2, range = [[-1,8],[-10,15]], norm=colors.PowerNorm(0.5), zorder=0.5)
